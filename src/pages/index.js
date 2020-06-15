c1aa8284be7d29baa0f45d5d39adbc413c50af48
ridwanfirdaus96 <va.rhenka@gmail.com>
2020-06-15 10:19:51 +0700

    Added Feature Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Container } from '../components'
+import { Container, FeatureImage } from '../components'
 
 const IndexPage = () => {
   return (
     <Container>
-      <div>Hello World</div>
+      <FeatureImage />
     </Container>
   )
 }
