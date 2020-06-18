a199c9a6ba823f7564d3bac88899a53b2c5ab3b0
ridwanfirdaus96 <va.rhenka@gmail.com>
2020-06-18 14:36:30 +0700

    Created a content Card Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Container, FeatureImage } from '../components'
+import { Container, FeatureImage, Content, ContentCard } from '../components'
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date='March 24, 2020'
+          title='My Fourth Post... So Long For Now'
+          excerpt='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit nisl, leo et. Vitae hendrerit vivamus cum ullamcorper hendrerit et arcu, elit ornare.'
+          slug='/test'
+        />
+      </Content>
     </Container>
   )
 }
